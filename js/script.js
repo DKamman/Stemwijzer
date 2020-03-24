4efae9a08a4fd868628ca4c02748357b0dab514b
DKamman <42833740+DKamman@users.noreply.github.com>
2020-03-24 14:16:08 +0100

    start

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
 function startStemWijzer () {
     console.log(subjects);
+    var start = document.getElementById('start');
+    start.remove();
+
+    var content = document.getElementById('content');
+    var title = document.createElement('H1');
+    var titleText = document.createTextNode('Test');
+
+    var statement = document.createElement('P');
+    var statementText = document.createTextNode('Statement Test');
+    
+    title.appendChild(titleText);
+    content.appendChild(title);
+
+    statement.appendChild(statementText)
+    content.appendChild(statement);
 }

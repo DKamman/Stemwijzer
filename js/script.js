48cbdc9471966ea5d4a48b2e9c1da9b033c9fa6e
DKamman <42833740+DKamman@users.noreply.github.com>
2020-03-26 02:49:48 +0100

    Updated agree disagree neutral and previous button

--- js/script.js
+++ js/script.js
@@ -1,18 +1,78 @@
+var anwsers = [];
+var index = 0;
+
 function startStemWijzer () {
     console.log(subjects);
+
     var start = document.getElementById('start');
     start.remove();
 
-    var content = document.getElementById('content');
-    var title = document.createElement('H1');
-    var titleText = document.createTextNode('Test');
+    var list = document.getElementById('list');
+    list.style.display = 'block';
 
-    var statement = document.createElement('P');
-    var statementText = document.createTextNode('Statement Test');
-    
-    title.appendChild(titleText);
-    content.appendChild(title);
-
-    statement.appendChild(statementText)
-    content.appendChild(statement);
+    displayStatement(0);
 }
+
+function displayStatement(index) {
+    var title = document.getElementById('title');
+    var statement = document.getElementById('statement');
+    title.innerHTML = (index+1).toString() + '. ' + subjects[index].title;
+    statement.innerHTML = subjects[index].statement;
+}
+
+function agreeQuestion() {
+    anwsers[index] = 'Eens';
+    indexCheck();
+    displayStatement(index);
+
+    console.log(index);
+
+    checkEnd()
+}
+
+function disagreeQuestion() {
+    anwsers[index] = 'Oneens';
+    indexCheck();
+    displayStatement(index);
+
+    console.log(index);
+
+    checkEnd()
+}
+
+function neutralQuestion() {
+    anwsers[index] = 'Neutraal';
+    indexCheck();
+    displayStatement(index);
+
+    console.log(index);
+
+    checkEnd()
+}
+
+function previousQuestion() {
+    if (index > 0) {
+        index--;        
+    }
+
+    displayStatement(index);
+
+    console.log(index);
+}
+
+function indexCheck() {
+    if (index < 29) {
+        index++;
+    }
+}
+
+
+function checkEnd() {
+    if (index == '29') {
+        console.log('This Is The End');
+
+    }
+}
+
+
+
